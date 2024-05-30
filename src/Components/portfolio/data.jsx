e7e61989038427f3a755718781ace10eb8491f45
manishN1999 <mnair1999@yahoo.com>
2024-05-30 19:45:50 +0530

    modified images and jsx file

--- src/Components/portfolio/data.jsx
+++ src/Components/portfolio/data.jsx
@@ -5,9 +5,9 @@ import Image3 from "./projects/image3.png";
 // import Image5 from "./projects/image5.png";
 // import Image6 from "./projects/image6.png";
 // import Image8 from "./projects/image8.png";
-import Image9 from "./projects/image9.png";
+// import Image9 from "./projects/image9.png";
 // import Image7 from "./projects/image7.png";
-import Image11 from "./projects/image11.png";
+// import Image11 from "./projects/image11.png";
 // import Image10 from "./projects/image10.png";
 
 export const projectsData = [
@@ -16,29 +16,29 @@ export const projectsData = [
     image: Image2,
     title: "Modern Landing Page",
     category: "html & css",
-    // Live:"https://social-proof-section-mu-nine.vercel.app/",
+    Live:"https://manish-modern-landing-page.vercel.app/",
   },
   {
     id: 2,
     image: Image3,
     title: "To-Do List",
     category: "react",
-    // Live: "https://product-dashboard-12.vercel.app/",
+    Live: "https://task-sheduler.vercel.app/",
   },
-  {
-    id: 3,
-    category: "Python",
-    title: "Air-Canvas",
-    image: Image11,
-    // Live:"https://horoscope-app-topaz.vercel.app/",
-  },
-  {
-    id: 4,
-    category: "react",
-    title: "Pintrest",
-    image: Image9,
-    // Live:"https://testimonials-grid-section-d47x.vercel.app/",
-  },
+  // {
+  //   id: 3,
+  //   category: "Python",
+  //   title: "Air-Canvas",
+  //   image: Image11,
+  //   // Live:"https://horoscope-app-topaz.vercel.app/",
+  // },
+  // {
+  //   id: 4,
+  //   category: "react",
+  //   title: "Pintrest",
+  //   image: Image9,
+  //   // Live:"https://testimonials-grid-section-d47x.vercel.app/",
+  // },
 ];
 
 export const projectsNav = [
@@ -51,7 +51,7 @@ export const projectsNav = [
   {
     name: "html & css",
   },
-  {
-    name: "Python",
-  },
+  // {
+  //   name: "Python",
+  // },
 ];
